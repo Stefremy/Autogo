90257f0151f4e1822eccc05373cbd1a15d5ba68c
stef <stefano.remy@gmail.com>
2025-08-28 16:35:00 +0100

    Modify Google verification meta tag in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+// pages/_app.tsx
 import "../styles/globals.css";
 import "../styles/globals.scss";
 import { useRouter } from "next/router";
@@ -7,22 +7,23 @@ import type { UserConfig } from "next-i18next";
 import type { AppProps } from "next/app";
 import nextI18NextConfig from "../next-i18next.config.js";
 import { IndexNavbar } from "../components/IndexNavbar";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  // Hide IndexNavbar on /viaturas and /cars/[id]
   const hideNavbar =
     router.pathname === "/viaturas" || router.pathname.startsWith("/cars");
 
   return (
     <>
-      {/* ✅ Meta tag de verificação Google */}
       <Head>
+        {/* Google Search Console verification */}
         <meta
           name="google-site-verification"
-          content="google435fb1bad5f3fe11.html"
+          content="Gbbb61qlNK2IcN7uE1oKw8fwz2i5l-yCZazLR-I5uWU"
         />
       </Head>
-
       {!hideNavbar && <IndexNavbar />}
       <Component {...pageProps} />
     </>
