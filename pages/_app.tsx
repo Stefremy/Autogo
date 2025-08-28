19c0ff8ce75e5f9d50df0a96b488586e6e04f98f
stef <stefano.remy@gmail.com>
2025-08-28 16:29:49 +0100

    Add Google site verification meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import "../styles/globals.scss";
 import { useRouter } from "next/router";
@@ -6,17 +7,15 @@ import type { UserConfig } from "next-i18next";
 import type { AppProps } from "next/app";
 import nextI18NextConfig from "../next-i18next.config.js";
 import { IndexNavbar } from "../components/IndexNavbar";
-import Head from "next/head";   // ✅ importa o Head
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  // Hide IndexNavbar on /viaturas and /cars/[id]
   const hideNavbar =
     router.pathname === "/viaturas" || router.pathname.startsWith("/cars");
 
   return (
     <>
-      {/* ✅ Head global para todas as páginas */}
+      {/* ✅ Meta tag de verificação Google */}
       <Head>
         <meta
           name="google-site-verification"
